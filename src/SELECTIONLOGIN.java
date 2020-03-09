2a869fc7ae6ddac93269b430a6085af6cc0ab4f4
majid-85 <majidknml8595@gmail.com>
2020-03-09 09:41:29 -0700

    updated parent detailes

--- src/SELECTIONLOGIN.java
+++ src/SELECTIONLOGIN.java
@@ -34,6 +34,7 @@ public class SELECTIONLOGIN extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -45,32 +46,29 @@ public class SELECTIONLOGIN extends javax.swing.JFrame {
         jLabel1.setText("                 S S M P T C TIRUR");
         jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 40, 394, 39));
 
-        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/TEACHER 2.jpg"))); // NOI18N
-        jButton1.setText("jButton1");
+        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icons8-parenting-100.png"))); // NOI18N
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
             }
         });
-        jPanel1.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(125, 216, 100, 100));
+        jPanel1.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 230, 100, 100));
 
-        jButton2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/STUDENT 2.jpg"))); // NOI18N
-        jButton2.setText("jButton1");
+        jButton2.setIcon(new javax.swing.ImageIcon("C:\\Users\\Nabeel\\Downloads\\icons8-student-male-100.png")); // NOI18N
         jButton2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton2ActionPerformed(evt);
             }
         });
-        jPanel1.add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(413, 216, 100, 100));
+        jPanel1.add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 230, 100, 100));
 
-        jButton3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/PARANT 2.jpg"))); // NOI18N
-        jButton3.setText("jButton1");
+        jButton3.setIcon(new javax.swing.ImageIcon("C:\\Users\\Nabeel\\Downloads\\icons8-teacher-100.png")); // NOI18N
         jButton3.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton3ActionPerformed(evt);
             }
         });
-        jPanel1.add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(702, 216, 100, 100));
+        jPanel1.add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(700, 230, 100, 100));
 
         jLabel2.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
         jLabel2.setForeground(new java.awt.Color(255, 255, 255));
@@ -79,13 +77,18 @@ public class SELECTIONLOGIN extends javax.swing.JFrame {
 
         jLabel3.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
         jLabel3.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel3.setText("     STUDENT");
+        jLabel3.setText("      STUDENT");
         jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(413, 343, 100, 28));
 
         jLabel4.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
         jLabel4.setForeground(new java.awt.Color(255, 255, 255));
         jLabel4.setText("     TEACHER");
         jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(702, 343, 100, 28));
+
+        jLabel6.setFont(new java.awt.Font("Yu Gothic UI", 3, 24)); // NOI18N
+        jLabel6.setForeground(new java.awt.Color(51, 0, 255));
+        jLabel6.setText("       STUDENT INFORMATION");
+        jPanel1.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 90, 360, 30));
 
         jLabel5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/lg (1).jpg"))); // NOI18N
         jLabel5.setText("jLabel5");
@@ -111,17 +114,17 @@ public class SELECTIONLOGIN extends javax.swing.JFrame {
         dispose();
     }//GEN-LAST:event_jButton2ActionPerformed
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
-        new parentnew().setVisible(true);
-        dispose();
-    }//GEN-LAST:event_jButton1ActionPerformed
-
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         // TODO add your handling code here:
         new teachernew().setVisible(true);
         dispose();
     }//GEN-LAST:event_jButton3ActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        new parentnew().setVisible(true);
+        dispose();
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -167,6 +170,7 @@ public class SELECTIONLOGIN extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
     private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
--- src/parentnew.java
+++ src/parentnew.java
@@ -101,7 +101,8 @@ public class parentnew extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
+    new PARENTDETAILS().setVisible(true);
+        dispose();        // TODO add your handling code here:
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
